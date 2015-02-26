415cf91d1e41b5e7b939ab4394345105cc6b6b8e
Jonty Wareing <jonty@jonty.co.uk>
2015-02-26 12:16:03 +0000

    Remove unnessecary loop

--- pretascraper.py
+++ pretascraper.py
@@ -10,11 +10,9 @@ prets = []
 
 root = lxml.html.document_fromstring(root_page)
 links = root.xpath('//*[@id="content"]/table/tr/td[1]/a')
-for item in links:
-    prets.append('http://www.pret.com' + item.attrib['href'])
-
-for pret in prets:
+for link in links:
+    pret = link.attrib['href']
     print pret
-    req = requests.get(pret)
+    req = requests.get('http://www.pret.com' + pret)
     with open('raw_prets/' + os.path.split(pret)[1], 'w') as f:
         f.write(req.content)
